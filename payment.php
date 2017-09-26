fc14147dd989374442b9794df6baa8ccd0ae1a4a
Dan Wood <danwood@users.noreply.github.com>
2017-09-26 13:02:17 -0700

    use version from live website

--- payment.php
+++ payment.php
@@ -41,7 +41,10 @@ include('_header.php'); ?>
 
 
 
-<!-- This is the generic Glossy-Program form.  Just un-comment this whole section below and put in the title of the show, and we are set! -->
+<!-- This is the generic Glossy-Program form.  Just un-comment this whole section below and put in the title of the show, and we are set!
+
+-->
+
 
 
 
@@ -74,12 +77,11 @@ include('_header.php'); ?>
 </form>
 
 
-
-<!-- This is the lower-cost ad form - lower costs.  Just un-comment this whole section below and put in the title of the show, and we are set! -->
+<!-- This is the all-experience, non-glossy program ad form - lower costs.  Just un-comment this whole section below and put in the title of the show, and we are set! -->
 
 <!--
 
-<h3>Glossy Program Ad</h3>
+<h3>All Experience Program Ads</h3>
 
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
@@ -116,42 +118,42 @@ include('_header.php'); ?>
 
 <!-- This is the generic T-shirt form.  Just un-comment this whole section below and put in the title/description of the show, and we are set!
 
-
+<!--
 
 
 
 <h3>Mysterious Mainstage T-Shirt</h3>
 
 <p>
-	Cast members, buy a shirt for yourself, and extras for your family!
+Cast members, buy a shirt for yourself, and extras for your family!
 </p>
 <p>
-	You can order multiple shirts if you want — pick a size and add to cart, then before you check out,
-	go "back" to this page to add additional shirts to your cart.
+You can order multiple shirts if you want — pick a size and add to cart, then before you check out,
+go "back" to this page to add additional shirts to your cart.
 </p>
 <p>
-	There are two main styles:  Ladies (trimmer cut, with a V-neck) and Youth/Adult (unisex).
+There are two main styles:  Ladies (trimmer cut, with a V-neck) and Youth/Adult (unisex).
 </p>
 
 <p>
-	Shirt will be black, with design on the front and the back.
+Shirt will be black, with design on the front and the back.
 </p>
 
-	<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="9XB6R5FAWMLTJ">
 <table>
 <tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
-	<option value="Youth - Medium (8)">Youth - Medium (8) $10.00 USD</option>
-	<option value="Youth - Large (10-12)">Youth - Large (10-12) $10.00 USD</option>
-	<option value="Adult - Small">Adult - Small $10.00 USD</option>
-	<option value="Adult - Medium">Adult - Medium $10.00 USD</option>
-	<option value="Adult - Large">Adult - Large $10.00 USD</option>
-	<option value="Adult - XL">Adult - XL $10.00 USD</option>
-	<option value="Ladies - S">Ladies - S $10.00 USD</option>
-	<option value="Ladies - M">Ladies - M $10.00 USD</option>
-	<option value="Ladies - L">Ladies - L $10.00 USD</option>
-	<option value="Ladies - XL">Ladies - XL $10.00 USD</option>
+<option value="Youth - Medium (8)">Youth - Medium (8) $10.00 USD</option>
+<option value="Youth - Large (10-12)">Youth - Large (10-12) $10.00 USD</option>
+<option value="Adult - Small">Adult - Small $10.00 USD</option>
+<option value="Adult - Medium">Adult - Medium $10.00 USD</option>
+<option value="Adult - Large">Adult - Large $10.00 USD</option>
+<option value="Adult - XL">Adult - XL $10.00 USD</option>
+<option value="Ladies - S">Ladies - S $10.00 USD</option>
+<option value="Ladies - M">Ladies - M $10.00 USD</option>
+<option value="Ladies - L">Ladies - L $10.00 USD</option>
+<option value="Ladies - XL">Ladies - XL $10.00 USD</option>
 </select> </td></tr>
 </table>
 <input type="hidden" name="currency_code" value="USD">
@@ -159,7 +161,7 @@ include('_header.php'); ?>
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 
-
+-->
 
 
 
@@ -169,34 +171,6 @@ include('_header.php'); ?>
 							<div class="inlinebox">
 
 
-
-<!-- This is the generic All-Experiences form.  Just un-comment this whole section below and put in the title of the show, and we are set! -->
-
-
-<!--
-
-<h3>_________________ Program Ad</h3>
-
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="Y5865TW69NNKA">
-<table>
-<tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
-	<option value="Personal 1/8 page">Personal 1/8 page $20.00 USD</option>
-	<option value="Personal 1/4 page">Personal 1/4 page $35.00 USD</option>
-	<option value="Personal 1/2 page">Personal 1/2 page $65.00 USD</option>
-	<option value="Business 1/2 page">Business 1/2 page $100.00 USD</option>
-	<option value="Business 1/2 page with layout service">Business 1/2 page with layout service $125.00 USD</option>
-	<option value="Business Full page">Business Full page $200.00 USD</option>
-	<option value="Business Full page with layout service">Business Full page with layout service $225.00 USD</option>
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="USD">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
-
--->
 
 
 
