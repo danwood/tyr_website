87ff33e34de169ca49d3fdce840d77ffda997a70
Dan Wood <danwood@users.noreply.github.com>
2017-10-31 08:27:48 -0700

    template for new ‘private’ file

---
+++ _private.php
@@ -0,0 +1,24 @@
+<?php
+
+// The 'live' version of this file should be placed one level above the web root.  Fill it in with appropriate values
+
+$backstage_password = 'XXX';
+
+
+
+
+// BELOW LINES ARE UPDATED PROGRAMATICALLY FROM BACKSTAGE.  BE CAREFUL HAND-EDITING!
+
+$payment_glossy_program = TRUE;
+$payment_glossy_name = 'XXX';
+
+$payment_blackandwhite_program = TRUE;
+$payment_blackandwhite_name = 'XXX';
+
+$payment_tshirt = TRUE;
+$payment_tshirt_twosided = TRUE;
+$payment_tshirt_name = 'XXX';
+$payment_tshirt_color = 'XXX';
+$payment_tshirt_twosided = TRUE;
+
+?>
