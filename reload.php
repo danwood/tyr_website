afa342908d39bcc2d6bdc7f8d8eb49115619609a
Dan Wood <danwood@users.noreply.github.com>
2017-12-12 16:16:11 -0800

    clarify this

--- reload.php
+++ reload.php
@@ -6,6 +6,8 @@ include('_reload.php');	// not a function, and not called from a function, so al
 
 
 ?>
+<h1>Rebuild HTML pages</h1>
 <p>Rebuilding of the HTML pages is now done automatically when there is a change to the database.</p>
-<p>So there is no need to do this manually.</p>
-<p>However, this process is also done every night, so that each day's website shows fresh data.</p>
+<p>So there is generally no need to do this manually, unless the HTML has changed and this needs to be rebuilt.</p>
+<hr />
+<p>This process is also done every night, so that each day's website shows fresh data.</p>
