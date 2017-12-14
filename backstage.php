a84f4b467794631b4cf6c03c751264b61623a3fa
Dan Wood <danwood@users.noreply.github.com>
2017-12-13 16:01:22 -0800

    don’t need any more

--- backstage.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta http-equiv="refresh" content="0; url=http://<?php echo htmlspecialchars($_SERVER["HTTP_HOST"]); ?>/backstage/" />
-</head>
-<body>
-</body>
-</html>
