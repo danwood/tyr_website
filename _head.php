186265b5593b9863b572e451ba88f4f5c6717426
Dan Wood <danwood@users.noreply.github.com>
2017-12-15 15:27:02 -0800

    new cachebusters

--- _head.php
+++ _head.php
@@ -11,18 +11,18 @@
 	<meta name="geo.placename" content="Alameda" />
 	<!--[if gte IE 8]><!-->
 	<link href='http://fonts.googleapis.com/css?family=Homenaje%7CDroid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css' />
-	<link rel="stylesheet" href="<?php echo $root; ?>style/styles.css?md=20171212" />
-	<link rel="stylesheet" href="<?php echo $root; ?>style/grid.css?md=20171212" />
-	<link rel="stylesheet" href="<?php echo $root; ?>style/header.css?md=20171212" />
-	<link rel="shortcut icon" href="<?php echo $root; ?>icon/favicon.ico?md=20171212" type="image/x-icon" />
-	<link rel="apple-touch-icon" href="<?php echo $root; ?>icon/apple-touch-icon.png?md=20171212" />
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $root; ?>icon/apple-touch-icon-57x57.png?md=20171212" />
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $root; ?>icon/apple-touch-icon-72x72.png?md=20171212" />
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $root; ?>icon/apple-touch-icon-76x76.png?md=20171212" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $root; ?>icon/apple-touch-icon-114x114.png?md=20171212" />
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $root; ?>icon/apple-touch-icon-120x120.png?md=20171212" />
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $root; ?>icon/apple-touch-icon-144x144.png?md=20171212" />
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $root; ?>icon/apple-touch-icon-152x152.png?md=20171212" />
+	<link rel="stylesheet" href="<?php echo $root; ?>style/styles.css?cb=20171215" />
+	<link rel="stylesheet" href="<?php echo $root; ?>style/grid.css?cb=20171215" />
+	<link rel="stylesheet" href="<?php echo $root; ?>style/header.css?cb=20171215" />
+	<link rel="shortcut icon" href="<?php echo $root; ?>icon/favicon.ico?cb=20171215" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="<?php echo $root; ?>icon/apple-touch-icon.png?cb=20171215" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $root; ?>icon/apple-touch-icon-57x57.png?cb=20171215" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $root; ?>icon/apple-touch-icon-72x72.png?cb=20171215" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $root; ?>icon/apple-touch-icon-76x76.png?cb=20171215" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $root; ?>icon/apple-touch-icon-114x114.png?cb=20171215" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $root; ?>icon/apple-touch-icon-120x120.png?cb=20171215" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $root; ?>icon/apple-touch-icon-144x144.png?cb=20171215" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $root; ?>icon/apple-touch-icon-152x152.png?cb=20171215" />
 	<!--<![endif]-->
 
 	<!--[if lte IE 9]>
