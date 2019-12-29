96558df78e5318739f04e445035cfbacd2d17b2d
Dan Wood <danwood@users.noreply.github.com>
2019-12-29 12:02:56 -0800

    updated button#1

--- tuition.php
+++ tuition.php
@@ -52,13 +52,13 @@ if ($payment_tuition || $payment_tuition_2) {
 <h3>Tuition for <?php echo htmlspecialchars($payment_tuition_name); ?></h3>
 <p>You must have already registered for the class.</p>
 
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="S23KQJ329QXAA">
+<input type="hidden" name="hosted_button_id" value="GXNEDDBY42QN4">
 <table>
 <tr><td><input type="hidden" name="on0" value="TUITION for (Student's name)">Student's name</td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
 </table>
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 
