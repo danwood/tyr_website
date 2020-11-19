e1be08cc7ab14a709a31971e8a6be3e8ef68e03f
Dan Wood <danwood@users.noreply.github.com>
2020-11-18 18:01:19 -0800

    extra donate buttons

--- _index.main.php
+++ _index.main.php
@@ -17,6 +17,14 @@
 										&nbsp;&nbsp; <a href="about.<?php echo currentExtension(); ?>">More about us</a>
 
 									</p>
+									<p>
+											<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="D9YMBDWGUL9SY">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+									</p>
 								</div>
 <?php
 if (count($currentShows) > 0)
